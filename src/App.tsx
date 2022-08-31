66d5ef83b423656f69e1c8bba830891c055ce62e
Felipe Couto <felipecoutodarocha@gmail.com>
2022-08-31 19:48:34 -0300

    Ajuste no localStorage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GlobalStyles from './global/GlobalStyles';
 import Home from './pages/Home/Home';
@@ -5,8 +6,21 @@ import Roadmap from './pages/Roadmap/Roadmap';
 import Details from './pages/Details/Details';
 import FormAdd from './pages/AddFeedback/FormAdd';
 import FormEdit from './pages/EditFeedback/FormEdit';
+import data from './utils/data';
 
 const App = () => {
+
+  useEffect(()=>{
+    const isStoraged = JSON.parse(localStorage.getItem('data')!)
+    //const storagedData = JSON.parse(localStorage.getItem('data')!);
+
+    if(!isStoraged){
+      localStorage.setItem('data', JSON.stringify(data));
+    }
+
+  },[])
+
+
   return (
     <>
       <GlobalStyles />
